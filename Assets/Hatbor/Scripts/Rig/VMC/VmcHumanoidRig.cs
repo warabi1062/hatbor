44c9fa07c77691d9ffaf70e110f15597d9686294
warabi1062 <kohei.2126@gmail.com>
2026-01-20 22:56:27 +0900

    fix position

--- Assets/Hatbor/Scripts/Rig/VMC/VmcHumanoidRig.cs
+++ Assets/Hatbor/Scripts/Rig/VMC/VmcHumanoidRig.cs
@@ -1,3 +1,4 @@
+using Hatbor.Config;
 using Hatbor.VMC;
 using UniHumanoid;
 using UnityEngine;
@@ -11,11 +12,13 @@ namespace Hatbor.Rig.VMC
         const float SmoothFactor = 0.05f;
 
         readonly VmcServer vmcServer;
+        readonly FixedCameraConfig cameraConfig;
 
         [Inject]
-        public VmcHumanoidRig(VmcServer vmcServer)
+        public VmcHumanoidRig(VmcServer vmcServer, FixedCameraConfig cameraConfig)
         {
             this.vmcServer = vmcServer;
+            this.cameraConfig = cameraConfig;
         }
 
         void IHumanoidRig.Update(Vrm10Instance instance)
@@ -34,12 +37,26 @@ namespace Hatbor.Rig.VMC
                 if (bone == HumanBodyBones.Hips)
                 {
                     t.localRotation = Quaternion.Slerp(t.localRotation, pose.rotation, SmoothFactor);
+                    ApplyFaceToCamera(t);
                 }
                 else
                 {
                     t.localRotation = Quaternion.Slerp(t.localRotation, pose.rotation, SmoothFactor);
                 }
             }
+        }
+
+        void ApplyFaceToCamera(Transform hips)
+        {
+            var cameraPos = cameraConfig.CameraPosition.Value;
+            var hipsPos = hips.position;
+
+            var direction = new Vector3(cameraPos.x - hipsPos.x, 0f, cameraPos.z - hipsPos.z);
+            if (direction.sqrMagnitude < 0.001f) return;
+
+            var targetYRotation = Quaternion.LookRotation(direction).eulerAngles.y;
+            var currentRotation = hips.rotation.eulerAngles;
+            hips.rotation = Quaternion.Euler(currentRotation.x, targetYRotation, currentRotation.z);
         }
     }
 }
